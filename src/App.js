433e4881df8c8b297623309e1e304d6b32eb7977
sadeshwar94 <sadeshwarnakulasooriya@gmail.com>
2021-03-08 10:49:58 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello , This a testing </h1>
+      {/* This is a button comment */}
+      <button> Master Wooly</button>
+
+      <button> Ken the Ninga </button>
+    </div>
+  );
+}
